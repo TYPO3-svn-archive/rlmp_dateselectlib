3219e777d04e91ed3d83510f62567f1c5b232b43
robert <robert@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-30 08:50:32 +0000

    Initial check in of the current version (0.1.7)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rlmp_dateselectlib/trunk@12452 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "rlmp_dateselectlib"
+#
+# Auto generated 30-09-2008 10:44
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Date selector library',
+	'description' => 'Provides a DHTML configurable calendar as a date selector. This is a library for usage in your own PHP scripts.',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '0.1.7',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Robert Lemke',
+	'author_email' => 'robert@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '3.7.0-0.0.0',
+			'php' => '4.1.0-0.0.0',
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"12a8";s:19:"calendar-system.css";s:4:"1317";s:17:"calendar-typo3.js";s:4:"8e5f";s:30:"class.tx_rlmpdateselectlib.php";s:4:"71ae";s:12:"ext_icon.gif";s:4:"e4e2";s:24:"ext_typoscript_setup.txt";s:4:"789f";s:14:"locallang.2php";s:4:"42e3";s:13:"locallang.php";s:4:"715d";s:14:"doc/manual.sxw";s:4:"25a9";}',
+);
+
+?>
